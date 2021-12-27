cf7a041426d70b7615703750c6af820a3c1b6558
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-27 18:27:24 +0100

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Polymorphisme
+{
+    class Book
+    {
+        protected int ISBN;
+        protected string Title;
+        protected string Author;
+        protected float Price;
+        protected int numberPage;
+
+        public Book(int iSBN, string title, string author, float price, int numberPage)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Author = author;
+            Price = price;
+            if (numberPage >= 20)
+            this.numberPage = numberPage;
+        }
+
+        public Book()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()
+                + "\nISBN : " + this.ISBN 
+                + "\nTitle: " + this.Title 
+                + "\nAuthor: " + this.Author 
+                + "\nPrice$ : " + this.Price 
+                + "\nNumberPages: " + this.numberPage;
+        }
+
+        // Surcharge 
+
+        public bool Comparer (Book b)
+        {
+            bool res = false;
+
+            if ( this.ISBN == b.ISBN
+                 && this.numberPage == b.numberPage
+                 && this.Title == b.Title
+                 && this.Price == b.Price
+                 && this.Author == b.Author)
+                res = true;
+
+            return res;
+        }
+
+        public bool Comparer(string Title)
+        {
+            bool res = false;
+
+            if (this.Title == Title)
+                res = true;
+
+            return res;
+        }
+
+        public bool Comparer(float Price)
+        {
+            bool res = false;
+
+            if (this.Price == Price)
+                res = true;
+
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TP_Polymorphisme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book(121, "C# in Depth", "John Mack", (float)12.3, 450);
+
+            Console.WriteLine(b.ToString());
+
+            if (b.Comparer("C# in Depth"))
+                Console.WriteLine("it's the book title");
+            else
+                Console.WriteLine("it's Not the book title");
+
+            if (b.Comparer((float)12.3))
+                Console.WriteLine("it's the same price ");
+            else
+                Console.WriteLine("it's not the same price");
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
